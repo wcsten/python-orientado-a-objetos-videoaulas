d40a64a86edabdff996a139cf043ffd05ec8fc0a
Gabriel Stain de Souza <gabriel_sten@hotmail.com>
2017-03-20 12:41:53 -0300

    add class Pessoa in aula08

--- oo_python/aula08-multiplas-classes.py
+++ oo_python/aula08-multiplas-classes.py
@@ -8,10 +8,30 @@ class Animal:
 		print('Olá sou um cachorro e sei falar')
 
 	def andar(self):
-		print('Estou andando sobre {} tantas patas'.format(self.num_de_patas))
+		print('Estou andando sobre {} patas'.format(self.num_de_patas))
 	
 	def amamentar(self):
 		if self.genero.lower() [0] == 'm':
 			print('Machos não amamentam')
 			return
-		print('Amamentando meu filhote')		
+		print('Amamentando meu filhote')	
+
+Rex = Animal('Preto', 'masculino', 4)	
+
+Rex.falar()
+Rex.andar()
+Rex.amamentar()
+
+class Pessoa(Animal):
+	def __init__(self, cor, genero, andar, cabelo):
+		super(Pessoa, self).__init__(cor, genero, andar)
+		self.cabelo = 'Castanho'
+
+	def cor_cabelo(self):
+		print('A cor do cabelo é {}'.format(self.cabelo))
+
+Gabriel = Pessoa('branco', 'masculino', 2, 'castanho')
+
+Gabriel.andar()
+Gabriel.amamentar()
+Gabriel.cor_cabelo()
